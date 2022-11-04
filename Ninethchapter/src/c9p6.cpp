45a87cd84f243965f5fd12bbbc456e1275c33523
Konstantin Dietrich <Konst.Dietrich@gmail.com>
2022-11-04 15:42:52 +0100

    Done the pointer stuff

--- Ninethchapter/src/c9p6.cpp
+++ Ninethchapter/src/c9p6.cpp
@@ -21,7 +21,7 @@
 // address
 //
 // "Pointers":
-// A pointer is an object that holds a memory address(typically if another variable)
+// A pointer is an object that holds a memory address(typically of another variable)
 // as its value. This allows us to store the address of some other objects to use later.
 // An uninitialized pointer holds a garbage address that when dereferenced will
 // lead to undefined behavior
@@ -33,16 +33,101 @@
 // pointed at. Modifying this (*ptr = 6;) changes the value of the object being
 // pointed at.
 //
+// Both pointers and references behave similarly, because they both provide a way
+// to access another object, differences are:
+//  - with pointers one needs to explicitly get the address to point at, this
+//    happens implicitly with references
+//  - References must be initialized, pointers do not (but should be)
+//  - references are not objects, pointer are
+//  - references can not be reseated, pointers can change what they are pointing at
+//  - references must always be bound to an object, pointers can point to nothing
+//  - references are "safe"(outside dangling references), pointers are inherently
+//    dangerous
+// The size of pointers is depending on the architecture the executeable is 
+// compiled for -- a 32-bit executable uses 32-bit memory addresses, therefore
+// a pointer is also 32 bits long (4 bytes). Likewise for 64-bit systems
+// We can also have dangling pointers
+//
+// Pointers are more flexible than references but also more dangerous.
+//
+// 9.7 Null pointers
+// Besides a memory address, a pointer can also hold a null value. That means it
+// is not pointing at anything. -> Null Pointer
+// "Best Practice": Value initialize your pointers (to be null pointers) if you
+// are not initializing them with the address of a valid object.
+// "Best practice": Use "nullptr" keyword whne you need a null pointer literal
+// for initialization, assignement, or passing a null pointer to a function.
+// Dereferencing a nullpointer leads to undefined behavior, this mistake is 
+// probably the most common one that leads to programs crashing in practice!!!
+//
+// There is a test to check whether a null pointer is in fact a nullpointer
+// See below, also pointers actually implicitly convert to boolean values,
+// therefore one does not need to explictily check using the nullptr keyword.
+// Instead we can simply check for the pointer e.g.: if (ptr) since a null 
+// pointer would implicitly convert to the boolean value false
+// Dangling pointers are best avoided by setting all pointers that are not
+// pointing to a valid object to be null pointers
+// "Best practice": A pointer should either hold the address of a valid object,
+// or be set to nullptr. That way we only need to test pointers for null, and
+// can assume any non-null pointer is valid.
+// It is the programmers responsibility to ensure that all pointers that point
+// to an obejct that was destroyed are properly set to null pointer!!!
+//
+// As dicussed pointers and references both give you the ability to access some
+// other object indirectly. Pointers can additionally change what they are pointing
+// at and can be pointed at NULL. This makes pointer quite dangerous since the first
+// thing makes creating dangling pointers very easy and the second thing could
+// when dereferenced crash the program.
+// "Best practice": Favor references over pointers unless the additional
+// capabilities provided by pointers are needed.
+//
+// 9.8 Pointers and const
+// To declare a pointer to a const value, the const keyword has to be used before
+// the pointer's data type.
+// The const pointers value can not be changed, the const pointers address however
+// can be changed. It could also be pointed at a non const value. In such a case
+// the pointer would when dereferenced still not allow to change its value.
+// e.g.: const int* ptr { &x };
+//
+// There is also the possibility to make the pointer itself constant:
+// e.g.: int* const ptr { &x }; 
+// It must be initialized and the address it contains can not be changed. But
+// since the value it points at is not const the value can be changed by 
+// dereferencing the pointer.
+//
+// Const pointer to const value:
+// e.g.: const int* const ptr { &x };
+// This can not be changed at all, we can only dereference it to get the value
+// it is pointing at.
 //
 //
 int main()
 {
+   
+    int* ptr1 { nullptr }; // this is now a null pointer
+
     int x{ 5 };
     std::cout << x << '\n'; // print the value of variable x
     std::cout << &x << '\n'; // print the address of variable x
     std::cout << *(&x) << '\n'; // print the value at the memory address of variable
                                 // x (parantheses not required, but make it easier to read)
     int& ref { x }; // a reference to an integer (bound to x)
-    int* ptr; // a pointer to an integer
+    int* ptr { &x }; // a pointer to an integer
+    
+    std::cout << "ref to x: " << ref << '\n';
+    std::cout << "ptr to x: " << ptr << '\n';
+
+    // test to check whether the pointer is indeed a nullpointer or not
+    if (ptr == nullptr) // explicit test for equivalence
+        std::cout << "ptr is null\n";
+    else
+        std::cout << "ptr is non-null\n";
+
+    int* nullPtr {};
+    std::cout << "nullPtr is " << (nullPtr==nullptr ? "null\n" : "non-null\n");
+
+
+
+
     return 0;
 }
