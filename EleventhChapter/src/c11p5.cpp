8f0847c3661ac4b708ef4aa0630d2d4b0a5a1dbf
Konstantin Dietrich <Konst.Dietrich@gmail.com>
2023-01-10 21:40:07 +0100

    MacTransition

--- EleventhChapter/src/c11p5.cpp
+++ EleventhChapter/src/c11p5.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <iterator>
 
 
 // 11.5 Multidimensional Arrays
 // The elements of an array can be any data type including arrays, such an array
 // is called a multidimensional array
 //
-
+// 11.6 C-style strings
+// a c-style string is an array of characters that uses a "null terminator" which
+// is a special character '\0' used to indicate the end of a string
+// that is why they are also called a null terminated string
+//
+//
 
 int main()
 {
@@ -44,5 +50,27 @@ int main()
     int array2[2][3][4];
     // when dealing with such arrays there is no intuitive way to initialize them
     // in such a case values are usually later assigned in loops
+    //
+    
+    // to initialize a c-style string one simply uses a char array:
+    char myString[]{ "string" };
+    // c++ automatically adds a null terminator to the length of the array, therfore
+    // eventhough there are only 6 letters this is an array with size 7
+    //
+    // using c-style string with cin:
+
+    char name[255]{};
+    std::cout << "Enter your name: ";
+    std::cin.getline(name, std::size(name));
+    std::cout << "You entered: " << name << "\n";
+    // getline will discard everything after the 255 symbol leaving room for 
+    // the null terminator, this way one ensures that no undefined behavior happens
+    // if the user enters more than 255 characters, whether it might have been
+    // accidentally or maliciously
+    // The header <cstring> contains loads of functions to manipulate cstyle strings
+    // But "Best Practice": Use std::string or std::string_view instead of 
+    // c-style strings
+
+
     return 0;
 }
